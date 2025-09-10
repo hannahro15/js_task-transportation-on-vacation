811a785f34ef71b782cac991bc09785027a4f656
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-10 22:10:29 +0100

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const car = 40;
+
+  if (days >= 7) {
+    return car * days - 50;
+  } else if (days >= 3) {
+    return car * days - 20;
+  } else {
+    return car * days;
+  }
 }
 
 module.exports = calculateRentalCost;
