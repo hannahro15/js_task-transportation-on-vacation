bdea0db34fb632631911b19fc6f65e77f521efc5
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-09-10 22:44:17 +0100

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,10 +7,12 @@ function calculateRentalCost(days) {
   const DAILY_RATE = 40;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM_THRESHOLD = 7;
+  const SHORT_TERM_THRESHOLD = 3;
 
-  if (days >= 7) {
+  if (days >= LONG_TERM_THRESHOLD) {
     return DAILY_RATE * days - LONG_TERM_DISCOUNT;
-  } else if (days >= 3) {
+  } else if (days >= SHORT_TERM_THRESHOLD) {
     return DAILY_RATE * days - SHORT_TERM_DISCOUNT;
   } else {
     return DAILY_RATE * days;
